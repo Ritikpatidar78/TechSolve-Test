db98ec0a9a7557386a76baa0251b2b014fb3f770
Ritik patidar <ritikpatidar7898634330@gmail.com>
2024-08-05 15:44:16 +0530

    deafult value

--- backend/Models/Record.js
+++ backend/Models/Record.js
@@ -4,9 +4,9 @@ const  mongoose  = require("mongoose");
 // mongoose scheme for database
 const RecordSchema = mongoose.Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
-    phone: {type: String, required: true},
-    address: {type: String, required: true},
+    email: {type: String, required: true,default: ""},
+    phone: {type: String, required: true, default: ""},
+    address: {type: String, required: true , default: ""},
 },{
     timestamps: true,
 })
